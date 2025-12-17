8d03516c7157ecb6b081f172c34f8fd3d7ea6c4d
MohamedBarrak3 <barrakmohamed485@gmail.com>
2025-12-17 23:39:00 +0100

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/config.js";
-
-export function generateToken(payload) {
-  return jwt.sign(payload, JWT_SECRET, {
-    expiresIn: JWT_EXPIRES_IN,
-  });
-}
